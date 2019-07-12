20cb7e797eddf67bdd18432d7f541927334cb0bf
Danny Tseng <dtfocus1025@gmail.com>
2019-07-11 22:41:56 -0700

    adjust bid controller

--- rabit/app/controllers/api/v1/bids_controller.rb
+++ rabit/app/controllers/api/v1/bids_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BidsController < ApplicationController
-    skip_before_action :authorized, only: [:create, :index]
+    skip_before_action :authorized, only: [:create, :index, :show, :update]
     def index
         bids = Bid.all
         render json: bids
@@ -10,7 +10,7 @@ class Api::V1::BidsController < ApplicationController
         #byebug
         if @bid.save
             #byebug
-            render json: { task: BidSerializer.new(@bid) }, status: :created
+            render json: { bid: BidSerializer.new(@bid) }, status: :created
         else
             render json: { error: 'failed to create bid' }, status: :not_acceptable
         end
@@ -22,8 +22,17 @@ class Api::V1::BidsController < ApplicationController
         render json: BidSerializer.new(bid)
     end
 
+    def update
+        bid = Bid.find_by(id: params[:id])
+        if bid.update(bid_params)
+            render json: BidSerializer.new(bid).to_serialized_json
+        else
+        #
+        end
+    end
+
     private
-    def task_params
+    def bid_params
         params.require(:bid).permit(:amount, :eta, :user_id, :task_id)
     end
 end
